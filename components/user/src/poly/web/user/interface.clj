664b125af3fc5beb0608376fd0b7639535293c8d
cpackard <6462409+cpackard@users.noreply.github.com>
2022-12-31 18:04:15 -0500

    add `secret` Integrant key for `user` component
    used for authentication with JWTs.

--- components/user/src/poly/web/user/interface.clj
+++ components/user/src/poly/web/user/interface.clj
@@ -1,1 +1,11 @@
-(ns poly.web.user.interface)
+(ns poly.web.user.interface
+  (:require
+   [integrant.core :as ig]))
+
+(defmethod ig/pre-init-spec ::secret
+  [_]
+  string?)
+
+(defmethod ig/init-key ::secret
+  [_ secret]
+  secret)
