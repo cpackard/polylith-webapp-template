b041425a26d132577d064c8130bc7791ab92cbe8
cpackard <6462409+cpackard@users.noreply.github.com>
2023-01-05 18:52:18 -0500

    refactor `auth` specs

--- components/auth/src/poly/web/auth/interface/spec.clj
+++ components/auth/src/poly/web/auth/interface/spec.clj
@@ -1,8 +1,26 @@
 (ns poly.web.auth.interface.spec
   (:require
-   [clojure.spec.alpha :as s]
-   [poly.web.auth.spec :as auth-spec]))
+   [clojure.spec.alpha :as s]))
 
-(s/def ::jwt auth-spec/jwt)
+(def ^:private base-64-re "[a-zA-Z0-9/+-_]+=*")
+(def ^:private jwt-re (re-pattern (apply format "%s\\.%s\\.%s" (repeat 3 base-64-re))))
 
-(s/def ::jwt-str? auth-spec/jwt-str?)
+(def ^:private sample-jwt-strs #{"eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIwIiwiaXNzIjoiandvdzgyNEB5YWhvby5jb20iLCJleHAiOjE2NzMzMDI3MDcsImlhdCI6MTY3MjY5NzkwN30.nR7ILJrHy37GTlOqbB168-Nzt_FShWRMCLJ9jVuXbFw"
+                                 "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxIiwiaXNzIjoiandvdzgyNEB5YWhvby5jb20iLCJleHAiOjE2NzMzMDI0MjEsImlhdCI6MTY3MjY5NzYyMX0.UUldu7iCG5Crkci0FNqD-ZrQIjCI6-1ITMkQIY1NkMg"})
+
+(def jwt-str? (s/with-gen (s/and string? #(re-matches jwt-re %))
+                #(s/gen sample-jwt-strs)))
+
+(s/def ::sub string?)
+(s/def ::iss string?)
+(s/def ::exp pos-int?)
+(s/def ::iat pos-int?)
+
+(def jwt (s/keys :req-un [::sub
+                          ::iss
+                          ::exp
+                          ::iat]))
+
+(s/def ::jwt jwt)
+
+(s/def ::jwt-str? jwt-str?)
