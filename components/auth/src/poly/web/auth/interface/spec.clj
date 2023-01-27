ff53f389e2ba9b23d83f4c08b4912a41100831fa
cpackard <6462409+cpackard@users.noreply.github.com>
2023-01-27 16:29:44 -0500

    update auth token for tests

--- components/auth/src/poly/web/auth/interface/spec.clj
+++ components/auth/src/poly/web/auth/interface/spec.clj
@@ -6,7 +6,7 @@
 (def ^:private jwt-re (re-pattern (apply format "%s\\.%s\\.%s" (repeat 3 base-64-re))))
 
 ; TODO: figure out a better way to auto-generate these values (tokens expire after 7 days)
-(def ^:private sample-jwt-strs #{"eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJzYW1wbGUyIiwiaXNzIjoic2FtcGxlMkBleGFtcGxlLmNvbSIsImV4cCI6MTY3NDA2NzA1OCwiaWF0IjoxNjczNDYyMjU4fQ.EVSs5DNO2qXphjkhLd65tpAbjZJwmC-u9DH8IyUZO-o"})
+(def ^:private sample-jwt-strs #{"eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhMSIsImlzcyI6ImFAZXhhbXBsZS5jb20iLCJleHAiOjE2NzUzODgwODgsImlhdCI6MTY3NDc4MzI4OH0.tcylNT53yoCer0wQBGDKj6zGUw66xRbEjpv1m-rm-Fo"})
 
 (def jwt-str? (s/with-gen (s/and string? #(re-matches jwt-re %))
                 #(s/gen sample-jwt-strs)))
