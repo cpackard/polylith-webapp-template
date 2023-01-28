3495924835cefa636eef3b1cc66e20a6d280b072
cpackard <6462409+cpackard@users.noreply.github.com>
2023-01-28 15:12:37 -0500

    add `user` util functions for working with HTTP routes

--- development/src/user.clj
+++ development/src/user.clj
@@ -8,6 +8,8 @@
    [integrant.core :as ig]
    [integrant.repl :refer [clear go halt init prep reset reset-all]]
    [integrant.repl.state :refer [system]]
+   [io.pedestal.http.route :as route]
+   [io.pedestal.http.route.definition.table :refer [table-routes]]
    [poly.web.auth
     [core :as auth-core]
     [interface :as auth]]
@@ -75,6 +77,67 @@
   []
   (::sql/db-pool system))
 
+;; Utility functions from http://pedestal.io/guides/developing-at-the-repl#developing-at-the-repl
+
+(defn print-routes
+  "Print our application's routes"
+  []
+  (route/print-routes (table-routes api/routes)))
+
+(defn named-route
+  "Finds a route by name"
+  [route-name]
+  (->> api/routes
+       table-routes
+       (filter #(= route-name (:route-name %)))
+       first))
+
+(defn print-route
+  "Prints a route and its interceptors"
+  [rname]
+  (letfn [(joined-by
+            [s coll]
+            (apply str (interpose s coll)))
+
+          (repeat-str
+            [s n]
+            (apply str (repeat n s)))
+
+          (interceptor-info
+            [i]
+            (let [iname  (or (:name i) "<handler>")
+                  stages (joined-by
+                          ","
+                          (keys
+                           (filter
+                            (comp (complement nil?) val)
+                            (dissoc i :name))))]
+              (str iname " (" stages ")")))]
+    (when-let [rte (named-route rname)]
+      (let [{:keys [path method route-name interceptors]} rte
+            name-line (str "[" method " " path " " route-name "]")]
+        (joined-by
+         "\n"
+         (into [name-line (repeat-str "-" (count name-line))]
+               (map interceptor-info interceptors)))))))
+
+(defn recognize-route
+  "Verifies the requested HTTP verb and path are recognized by the router."
+  [verb path]
+  (route/try-routing-for (table-routes api/routes) :prefix-tree path verb))
+
 (comment
   ;; (re)start system
-  (setup))
+  (setup)
+
+  ;; print all routes
+  (print-routes)
+
+  ;; find a specific route
+  (named-route :user-register)
+
+  ;; get detailed route info by handler name
+  (print-route :user-register)
+
+  ;; get detailed route info by verb and relative path
+  (recognize-route :post "/api/users"))
