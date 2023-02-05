e4ce6c72e5e20d91de9559b16da126a0696c20a0
cpackard <6462409+cpackard@users.noreply.github.com>
2023-02-05 18:02:52 -0500

    dynamically generate test tokens

--- components/auth/src/poly/web/auth/interface/spec.clj
+++ components/auth/src/poly/web/auth/interface/spec.clj
@@ -1,16 +1,20 @@
 (ns poly.web.auth.interface.spec
   (:require
    [clojure.spec.alpha :as s]
+   [clojure.spec.gen.alpha :as gen]
+   [poly.web.auth.core :as core]
+   [poly.web.config.interface :as cfg]
    [poly.web.spec.interface :as spec]))
 
 (def ^:private base-64-re "[a-zA-Z0-9/+-_]+=*")
 (def ^:private jwt-re (re-pattern (apply format "%s\\.%s\\.%s" (repeat 3 base-64-re))))
 
-; TODO: figure out a better way to auto-generate these values (tokens expire after 7 days)
-(def ^:private sample-jwt-strs #{"eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhMSIsImlzcyI6ImFAZXhhbXBsZS5jb20iLCJleHAiOjE2NzUzODgwODgsImlhdCI6MTY3NDc4MzI4OH0.tcylNT53yoCer0wQBGDKj6zGUw66xRbEjpv1m-rm-Fo"})
-
 (def jwt-str? (s/with-gen (s/and string? #(re-matches jwt-re %))
-                #(s/gen sample-jwt-strs)))
+                #(gen/fmap (fn [[email username secret]]
+                             (core/generate-token email username secret))
+                           (gen/tuple (s/gen spec/email?)
+                                      (s/gen spec/str-min-len-20?)
+                                      (s/gen #{cfg/default-secret-value})))))
 
 (s/def ::sub string?)
 (s/def ::iss string?)
