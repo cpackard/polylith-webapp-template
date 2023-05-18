2e75b7e783df57fd745175849f36f14df313f089
cpackard <6462409+cpackard@users.noreply.github.com>
2023-05-18 08:38:31 -0400

    cleanup examples in gpg core

--- components/gpg/src/poly/web/gpg/core.clj
+++ components/gpg/src/poly/web/gpg/core.clj
@@ -4,41 +4,39 @@
     [message :as pgp-msg]
     [core :as pgp]
     [keyring :as keyring]]
+   [clojure.java.io :as io]
    [clojure.string]))
 
 (defn load-pubkey
   "Load the public key saved in the given file."
   [keyfile]
   (let [pub-keyring-coll (keyring/load-public-keyring keyfile)
-        pub-keyring (first pub-keyring-coll)]
+        pub-keyring      (first pub-keyring-coll)]
     (first pub-keyring)))
 
 (defn load-privkey
   "Load the private key saved in the given file and encrypted with passphrase."
   [pubkey passphrase keyfile]
   (let [priv-keyring-coll (keyring/load-secret-keyring keyfile)
-        priv-keyring (first priv-keyring-coll)
-        secret-key (keyring/get-secret-key priv-keyring (pgp/hex-id pubkey))]
+        priv-keyring      (first priv-keyring-coll)
+        secret-key        (keyring/get-secret-key priv-keyring (pgp/hex-id pubkey))]
     (pgp/unlock-key secret-key passphrase)))
 
 (defn encrypt
   "Encrypt the given content "
   [content pubkey]
-  (pgp-msg/encrypt
-   content pubkey
-   :format :utf8
-   :cipher :aes-256
-   :compress :zip
-   :armor true))
+  (pgp-msg/encrypt content
+                   pubkey
+                   :format :utf8
+                   :cipher :aes-256
+                   :compress :zip
+                   :armor true))
 
 (defn save-message!
   "Save the given message to the file located at f."
   [message f]
-  (spit f message))
-
-(comment
-  (def content "postgresql://my_user:Test123!@localhost/my_db")
-  (save-message! (encrypt content pubkey) "~/db-file.gpg"))
+  (with-open [file (io/writer f)]
+    (.write file message)))
 
 (defn decrypt
   "Decrypt the given message using the private key."
@@ -46,7 +44,14 @@
   (pgp-msg/decrypt message privkey))
 
 (comment
-  (defn db-conn-str
-    [passphrase]
-    (decrypt (slurp "db-file.gpg") (load-privkey (load-pubkey) passphrase (load-pubkey "~/.gnupg/pub.gpg"))))
-  (println (db-conn-str (.readPassword (System/console) "GPG passphrase: "))))
+  (do
+    (println "Enter GPG Passphrase:")
+    (let [passphrase (read-line) ; Test123!
+          pubkey   (load-pubkey (io/as-file (io/resource "gpg/christian@example.com.pub.gpg")))
+          privkey  (load-privkey pubkey passphrase (io/as-file (io/resource "gpg/christian@example.com.secret.gpg")))
+          content  "postgresql://my_user:Test123!@localhost/my_db"
+          filename "db-file.gpg"]
+      (save-message! (encrypt content pubkey) filename)
+      (println "Encrypted file contents:")
+      (println (decrypt (slurp filename) privkey))
+      (io/delete-file filename true))))
